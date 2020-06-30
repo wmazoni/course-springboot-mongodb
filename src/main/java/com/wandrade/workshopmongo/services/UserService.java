4d0db99eb2a30e3cdabc82cf96295be9c3eb6b85
Wellington Andrade <wmazoni@protonmail.com>
2020-06-30 15:29:11 -0300

    Obtendo um usuário por id

--- src/main/java/com/wandrade/workshopmongo/services/UserService.java
+++ src/main/java/com/wandrade/workshopmongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.wandrade.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wandrade.workshopmongo.domain.User;
 import com.wandrade.workshopmongo.repository.UserRepository;
+import com.wandrade.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
