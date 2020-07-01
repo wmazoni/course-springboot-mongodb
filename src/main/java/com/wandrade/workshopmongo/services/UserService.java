603800eb19df8d8045cee4ba2038b929fbe6b175
Wellington Andrade <wmazoni@protonmail.com>
2020-06-30 23:09:15 -0300

    Inserção de usuário com POST

--- src/main/java/com/wandrade/workshopmongo/services/UserService.java
+++ src/main/java/com/wandrade/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wandrade.workshopmongo.domain.User;
+import com.wandrade.workshopmongo.dto.UserDTO;
 import com.wandrade.workshopmongo.repository.UserRepository;
 import com.wandrade.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,15 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
+	
+	
 
 }
