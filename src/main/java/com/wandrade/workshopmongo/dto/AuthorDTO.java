6e960f1d354fd37566c5afb853bd5adf7cdfe384
Wellington Andrade <wmazoni@protonmail.com>
2020-07-03 18:59:05 -0300

    Projeção dos dados do autor com DTO

---
+++ src/main/java/com/wandrade/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,34 @@
+package com.wandrade.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.wandrade.workshopmongo.domain.User;
+
+public class AuthorDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
