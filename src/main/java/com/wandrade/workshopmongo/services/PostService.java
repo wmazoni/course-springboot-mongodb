797d6c571dcd5ad576ea9f475ec593c9e1fcc5bd
Wellington Andrade <wmazoni@protonmail.com>
2020-07-03 22:06:46 -0300

    Obtendo um post por id

---
+++ src/main/java/com/wandrade/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.wandrade.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wandrade.workshopmongo.domain.Post;
+import com.wandrade.workshopmongo.domain.User;
+import com.wandrade.workshopmongo.repository.PostRepository;
+import com.wandrade.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
